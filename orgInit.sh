04204daaea43dd7049fa54d49d8741cd20cb1670
mshanemc <shane.mclaughlin@salesforce.com>
2019-03-20 08:58:14 -0500

    I know how to assign a permset

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
-sfdx force:user:permset:assign electron
+sfdx force:user:permset:assign -n electron
 sfdx force:org:open
